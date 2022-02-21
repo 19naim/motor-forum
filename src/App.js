06250cc0c0221690c056de59618b9c17ce71fd0a
Md Naim hossain arif <thk.naim@gmail.com>
2022-02-21 18:42:55 +0100

    add about page & protected route

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home'
 import ThreadList from './components/ThreadList'
+import Thread from './components/Thread'
 import Navbar from './components/ForumNavbar'
 import Footer from './components/ForumFooter'
-import Register from './pages/Register';
+import Register from './pages/Register'
 import Signin from './pages/Signin'
+import About from './pages/About'
 import MyPage from './pages/MyPage'
 import ProtectedRoute from './routes/ProtectedRoute'
 import ForumContextProvider from './contexts/ForumContextProvider'
+import UserContextProvider from './contexts/UserContextProvider'
 
 const App = () => {
   return (
     <div className="App">
+      <UserContextProvider>
       <ForumContextProvider>
-        <Router>   
-          <Navbar/>              
+        <Router>         
+          <Navbar/>       
             <div className="content">
-              <Routes>
-                <ProtectedRoute exact path='/mypage' component={MyPage} />    
-                <Route path="/" element={<Home/>} />                  
-                <Route path="/:id" element={<ThreadList/>} />  
-                <Route path="/register" element={<Register/>} />  
-                <Route path="/signin" element={<Signin/>} />                             
-              </Routes>
-            </div>   
-          <Footer/>                 
+              <Switch>  
+                <ProtectedRoute exact path='/mypage' component={MyPage} />          
+                <Route exact path="/" component={Home} />                  
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/signin" component={Signin} />
+                <Route exact path="/about" component={About} /> 
+                <Route exact path="/:id" component={ThreadList} />
+                <Route exact path="/:id1/:id2" component={Thread} />                                   
+              </Switch>
+            </div>
+          <Footer/>                    
         </Router>
       </ForumContextProvider>
+      </UserContextProvider>
     </div>
   );
 }
--- src/components/ForumNavbar.js
+++ src/components/ForumNavbar.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from "react-router-dom";
+import { UserContext } from '../contexts/UserContextProvider'
 import {Container, Collapse, Navbar, NavbarToggler, Nav, NavItem} from 'reactstrap';
 
 const ForumNavbar= (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
+  const { user, setUser} = useContext(UserContext)
+
+  const logoutUser = async () => {
+    await fetch('/logout')
+    setUser(null)
+    localStorage.removeItem('token');
+  }
 
   return (
     <Container className="px-0">
       <Navbar light expand="md" className="bg-warning bg-gradient rounded-bottom shadow mb-2">
-        <Link to="/" className="mr-auto navbar-brand text-datk font-weight-bold mb-0 pt-2 pb-0"><h3>MOTOR FORUM</h3></Link>
+        <Link to="/" className="mr-auto navbar-brand text-datk font-weight-bold mb-0 pt-2 pb-0"><h3>MRIDU FORUM</h3></Link>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
+            {!user?(
             <>
               <NavItem>
                 <Link className="text-dark mr-4" to="/register">REGISTER</Link>
@@ -21,14 +30,16 @@ const ForumNavbar= (props) => {
                 <Link className="text-dark" to="/signin">SIGN IN</Link>
               </NavItem>
             </>
+            ):(
             <>
               <NavItem>
                 <Link className="text-dark mr-4" to="/mypage">MY PAGE</Link>
               </NavItem>
               <NavItem>
-                <Link className="text-dark" to="/"> LOGOUT</Link>
+                <Link className="text-dark" onClick={logoutUser} to="/"> LOGOUT</Link>
               </NavItem>
             </>
+            )}
           </Nav>          
         </Collapse>
       </Navbar>
